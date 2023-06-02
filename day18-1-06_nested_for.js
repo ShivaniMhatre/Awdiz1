cd8b5667ff58f33ffabbb03451fc345af750e771
ShivaniMhatre <shivanikatkar97@gmail.com>
2023-06-02 22:44:31 +0530

    Update day18-1-06_nested_for.js

--- day18-1-06_nested_for.js
+++ day18-1-06_nested_for.js
@@ -10,34 +10,34 @@
 //     }
 // }
 
-// Q 2. Find Two Number From Array Which Has Addition Is Equal To Target
-// var a,b;
-// var arr = [3, 54, 34, 32, 2, 23, 39, 76, 17,18,55];
-// var target = 21;
-// for (a = 0; a < arr.length; a++) {
-//     for (b = a + 1; b < arr.length; b++) {
-//         if (arr[b] - arr[a]==target) {
-//              console.log(arr[a],arr[b],"Got It!!");
-//         }
-//         else if(arr[a] - arr[b]==target){
-//             console.log(arr[a],arr[b],"Got It!!");
-//         }
-//     }
-// }
+// Q 2. Find Two Number From Array Which Has Subtraction Is Equal To Target
+var a,b;
+var arr = [3, 54, 34, 32, 2, 23, 39, 76, 17,18,55];
+var target = 21;
+for (a = 0; a < arr.length; a++) {
+    for (b = a + 1; b < arr.length; b++) {
+        if (arr[b] - arr[a]==target) {
+             console.log(arr[a],arr[b],"Got It!!");
+        }
+        else if(arr[a] - arr[b]==target){
+            console.log(arr[a],arr[b],"Got It!!");
+        }
+    }
+}
 
 // Q 3. Find Three Number Which Has Addition Is Equal To Target
-var x, y, z;
-var target = 93;
-var arr = [1,3, 54, 34, 32, 89, 23, 39, 76, 91,1];
-for (x = 0; x < arr.length; x++) {
-    for (y = x + 1; y < arr.length; y++) {
-        for (z = y + 1; z < arr.length; z++) {
-            if (arr[x] + arr[y] + arr[z] == target) {
-                console.log(arr[x],"+",arr[y],"+", arr[z],"=",arr[x]+arr[y]+arr[z] )
-            } 
-            // console.log(arr[x],"+",arr[y],"+", arr[z],"=",arr[x]+arr[y]+arr[z] )
-        }
+// var x, y, z;
+// var target = 93;
+// var arr = [1,3, 54, 34, 32, 89, 23, 39, 76, 91,1];
+// for (x = 0; x < arr.length; x++) {
+//     for (y = x + 1; y < arr.length; y++) {
+//         for (z = y + 1; z < arr.length; z++) {
+//             if (arr[x] + arr[y] + arr[z] == target) {
+//                 console.log(arr[x],"+",arr[y],"+", arr[z],"=",arr[x]+arr[y]+arr[z] )
+//             } 
+            
+//         }
         
-    }
+//     }
 
-}
+// }
